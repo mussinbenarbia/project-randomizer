4cc6933c74b0b3f92cf20343e824c8f7a273ac52
mouhssinebenarbia <mouhssine.benarbia@gmail.com>
2021-05-19 23:00:14 +0900

    Added footer & navbar

---
+++ client/src/components/Footer.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<nav class="footer font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-center py-4 px-6 bg-white shadow sm:items-baseline w-full">
+    <div>
+      <span class="text-lg cursor-pointer no-underline text-grey-darkest hover:text-blue-dark ml-2">Made with <span class="heart">♥</span> in Japan</span>
+    </div>
+  </nav>
+
+
+<style>
+
+</style>
---
+++ client/src/components/Navbar.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<nav class="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full">
+    <div class="mb-2 sm:mb-0">
+      <span class="text-2xl no-underline text-grey-darkest hover:text-blue-dark">Project Randomizer</span>
+    </div>
+    <div>
+      <span class="text-lg cursor-pointer no-underline text-grey-darkest hover:text-blue-dark ml-2">About</span>
+    </div>
+  </nav>
+
+
+<style>
+
+</style>
