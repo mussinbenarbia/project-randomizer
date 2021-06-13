70228fc65b437fbfb8db8c1bb2be22bb028e263b
mussinbenarbia <mouhssine.benarbia@gmail.com>
2021-06-13 14:12:51 +0900

    moved seeder function in its own file

---
+++ seed_db.py
@@ -0,0 +1,66 @@
+# def add_projects():
+#     mongo.db.projects.insert_many([
+#         {
+#             'name': "Blogging App",
+#             'stack': ["vue", "python", "flask", "postgresql"],
+#             'cat': "web"
+#         },
+#         {
+#             'name': "Twitter Clone",
+#             'stack': ["react", "ruby", "rails", "mongodb"],
+#             'cat': "web"
+#         },
+#         {
+#             'name': "Music Player",
+#             'stack': ["kotlin"],
+#             'cat': "mobile"
+#         },
+#         {
+#             'name': "Todo App",
+#             'stack': ["swift"],
+#             'cat': "mobile"
+#         },
+#         {
+#             'name': "Quiz Game",
+#             'stack': ["angular"],
+#             'cat': "web"
+#         },
+#         {
+#             'name': "Dice Rolling Simulator",
+#             'stack': ["svelte"],
+#             'cat': "web"
+#         },
+#         {
+#             'name': "E-Commerce Site",
+#             'stack': ["svelte", "nodejs", "express", "postgresql" ],
+#             'cat': "web"
+#         },
+#         {
+#             'name': "Chat App",
+#             'stack': ["react", "firebase"],
+#             'cat': "web"
+#         },
+#     ])
+
+
+# def add_technologies():
+#     mongo.db.technologies.insert_many([
+#         {'name': "react", 'cat':"front"},
+#         {'name': "vue", 'cat':"front"},
+#         {'name': "angular", 'cat':"front"},
+#         {'name': "svelte", 'cat':"front"},
+
+#         {'name': "nodejs", 'cat':"back"},
+#         {'name': "ruby", 'cat':"back"},
+#         {'name': "python", 'cat':"back"},
+#         {'name': "django", 'cat':"back"},
+#         {'name': "rails", 'cat':"back"},
+#         {'name': "express", 'cat':"back"},
+        
+#         {'name': "kotlin", 'cat':"mobile"},
+#         {'name': "swift", 'cat':"mobile"},
+        
+#         {'name': "postgresql", 'cat':"db"},
+#         {'name': "mongodb", 'cat':"db"},
+#         {'name': "firebase", 'cat':"db"},
+#         ])
